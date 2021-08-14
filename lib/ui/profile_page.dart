a4c4e0280a93266df6306b8708b2f78e598cddb6
mejdi14 <mejdiclubiste@gmail.com>
2021-08-14 19:34:22 +0100

    even the avatar was not easy :(

--- lib/ui/profile_page.dart
+++ lib/ui/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -10,6 +12,35 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        top: true,
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topRight,
+                child: SvgPicture.asset(
+                  'assets/img/menu_dots.svg',
+                  height: 20,
+                  width: 20,
+                )),
+            SizedBox(
+              height: 90,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                height: 150,
+                width: 150,
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('assets/img/me.jpeg'),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

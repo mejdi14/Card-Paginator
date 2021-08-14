0f1c49361ea8846c21ec03c59dfe63ee81cc4f40
mejdi14 <mejdiclubiste@gmail.com>
2021-08-14 19:09:35 +0100

    here we go again

---
+++ android/app/src/main/kotlin/com/example/card_paginator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.card_paginator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:card_paginator/ui/profile_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProfilePage(),
+    );
+  }
+}
+
+
